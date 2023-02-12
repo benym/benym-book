3fc3204f26d69bedc367485296ccf99697f12b0e
benym <benyuanming@gmail.com>
2023-02-12 19:31:56 +0800

    feature: pwa支持

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,7 +13,7 @@ module.exports = {
     base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
     head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
         ['link', {rel: 'icon', href: '/img/favicon-two.ico'}], //favicons，资源放在public文件夹
-        ['link', {rel: 'manifest', href: '/pwa/manifest.json'}], // pwa
+        ['link', {rel: 'manifest', href: '/manifest.json'}], // pwa
         ['meta', {name: 'keywords', content: 'Java,benym,blog,knowledge'}],
         ['meta', {name: 'theme-color', content: '#ff954b'}], // 移动浏览器主题颜色
         ['link', { rel: 'stylesheet', href: 'https://at.alicdn.com/t/font_3077305_pt8umhrn4k9.css' }], // 用于文章浏览量，阿里矢量库
